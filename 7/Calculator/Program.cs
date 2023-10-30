072b87332f1c1d64feba95f9961032dcc10e6cf4
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-10-30 20:25:42 +0300

    Lab7 Done

---
+++ 7/Calculator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public class Calculator
+        {
+            public static double Add(double number1, double number2)
+            {
+                return number1 + number2;
+            }
+            public static double Sub(double number1, double number2)
+            {
+                return number1 - number2;
+            }
+            public static double Mult(double number1, double number2)
+            {
+                return number1 * number2;
+            }
+            public static double Div(double number1, double number2)
+            {
+                return number1 / number2;
+            }
+            
+            public static double Pow(double baseNumber, int exponent)
+            {
+                if (exponent == 0)
+                {
+                    return 1.0;
+                }
+
+                double result = 1.0;
+                for (int i = 0; i < Math.Abs(exponent); i++)
+                {
+                    result *= baseNumber;
+                }
+
+                if (exponent < 0)
+                {
+                    result = 1.0 / result;
+                }
+
+                return result;
+            }
+
+            public static double FindSquareRoot(double number, double epsilon = 0.00001)
+            {
+                if (number == 0)
+                {
+                    return 0;
+                }
+
+                double guess = number / 2.0;
+                double previousGuess = 0.0;
+
+                while (Math.Abs(guess - previousGuess) > epsilon)
+                {
+                    previousGuess = guess;
+                    guess = 0.5 * (guess + number / guess);
+                }
+
+                return guess;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 7/Lab7_TPO/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using lab7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace Lab7_TPO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Assert.AreEqual(4.0, Calculator.Program.Calculator.Add(2.0, 2.0));
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Assert.AreEqual(3.0, Calculator.Program.Calculator.Sub(5.0, 2.0));
+        }
+
+        [TestMethod]
+        public void TestMult()
+        {
+            Assert.AreEqual(6.0, Calculator.Program.Calculator.Mult(2.0, 3.0));
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            Assert.AreEqual(5.0, Calculator.Program.Calculator.Div(10.0, 2.0));
+        }
+
+        [TestMethod]
+        public void TestPow()
+        {
+            Assert.AreEqual(8.0, Calculator.Program.Calculator.Pow(2.0, 3));
+        }
+
+        [TestMethod]
+        public void TestPowWithNegativeDegree()
+        {
+            Assert.AreEqual(0.125, Calculator.Program.Calculator.Pow(2.0, -3));
+        }
+
+        [TestMethod]
+        public void TestFindSquareRoot()
+        {
+            Assert.AreEqual(2.0, Calculator.Program.Calculator.FindSquareRoot(4.0));
+        }
+
+        [TestMethod]
+        public void TestFindSquareRootOfZero()
+        {
+            Assert.AreEqual(0.0, Calculator.Program.Calculator.FindSquareRoot(0.0));
+        }
+
+        [TestMethod]
+        public void TestFindSquareRootWithCustomEpsilon()
+        {
+            Assert.IsNotNull(Calculator.Program.Calculator.FindSquareRoot(25.0, 0.1));
+        }
+
+        [TestMethod]
+        public void TestDivByZero()
+        {
+            Assert.IsNotNull(Calculator.Program.Calculator.Div(5.0, 0.0));
+        }
+    }
+
+}
