6b561e15640ddbf0e955df1cdaeb9485d462b154
Danila Kozlyakovsky <danik2003globin@gmail.com>
2023-11-12 19:59:35 +0300

    lab9 done

---
+++ Lab9/UnitTest1.cs
@@ -0,0 +1,57 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Text;
+
+namespace Lab9
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.wildberries.by/";
+            verificationErrors = new StringBuilder();
+
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Assert.That(verificationErrors.ToString(), Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestAddItemToBasket()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+
+            Thread.Sleep(5000);
+
+            IWebElement search = driver.FindElement(By.XPath("//*[@id=\"wbx-app\"]/header/div[1]/div[2]/div/div/div[1]/div/input"));
+
+            search.SendKeys("футболка");
+
+            search.SendKeys(Keys.Enter);
+
+
+            Thread.Sleep(1000);
+            Assert.AreEqual(driver.Title, "футболка");
+
+            Thread.Sleep(20000);
+        }
+    }
+}
